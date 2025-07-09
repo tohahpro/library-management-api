29ae4bfea08b362a576832332b64b3b8644f4b5f
Toha Hossain <tohahpro@gmail.com>
2025-07-09 10:28:07 +0600

    error handler setup

--- src/app.ts
+++ src/app.ts
@@ -15,4 +15,13 @@ app.get('/', (req: Request, res: Response)=>{
     res.send('Welcome to Library Management API');
 })
 
+
+// 404 error handler
+app.use((req: Request, res: Response) => {
+    res.status(404).json({
+        success: false,
+        message: 'Route not found',
+    });
+})
+
 export default app;
